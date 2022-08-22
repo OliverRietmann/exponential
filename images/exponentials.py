e928ec28d05935383220cf6fb7fe6f28684deffb
Oliver Rietmann <rioliver@student.ethz.ch>
2022-08-22 23:15:40 +0200

    Added images/exponentials.py

---
+++ images/exponentials.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+#import os
+#import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename_grow = "exponentials_grow.pdf" #sys.argv[1]
+filename_decay = "exponentials_decay.pdf" #sys.argv[2]
+
+def f(x, a, b):
+    return a * b**x
+
+x = np.linspace(-2.0, 2.0, 100)
+a = 1.0
+
+b_list = [4.0, 3.0, 2.0]
+b_label = ["$b=2$", "$b=3$", "$b=4$"]
+plt.figure()
+plt.title("$f(x)=b^x$")
+for b, label in zip(b_list, b_label):
+    y = f(x, a, b)
+    plt.plot(x, y, label=label)
+plt.xlabel(r"$x$")
+plt.ylabel(r"$f(x)$")
+plt.grid(True)
+plt.legend(loc="upper left", prop={'size': 16})
+plt.tight_layout()
+plt.savefig(filename_grow, bbox_inches='tight')
+plt.close()
+
+b_list = [1.0 / b for b in b_list]
+b_label = [r"$b=\frac{1}{2}$", r"$b=\frac{1}{3}$", r"$b=\frac{1}{4}$"]
+plt.figure()
+plt.title("$f(x)=b^x$")
+for b, label in zip(b_list, b_label):
+    y = f(x, a, b)
+    plt.plot(x, y, label=label)
+plt.xlabel(r"$x$")
+plt.ylabel(r"$f(x)$")
+plt.grid(True)
+plt.legend(loc="upper right", prop={'size': 16})
+plt.tight_layout()
+plt.savefig(filename_decay, bbox_inches='tight')
+plt.close()
